7fcf897a4114616eb11878624c2294aab5b6b05a
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-07 20:57:42 +0500

    add rubocop linter

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
 gem 'simple_form', '~> 5.0', '>= 5.0.3'
 gem 'devise', '~> 4.7', '>= 4.7.3'
-
+gem 'rubocop'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController 
 
-	private
+  private
 
-	def sign_up_params 
-		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
-	end
+  def sign_up_params
+  	params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+  end
 
-	def acount_update_params 
-		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
-	end
+  def acount_update_params
+  	params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+  end
 
 end
