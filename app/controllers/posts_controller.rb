4e49af81abf6dc4b188503131f3b2294c57980e4
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-02-07 21:06:11 +0500

    fix linter erros

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,13 +59,13 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:post)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:post)
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
-class RegistrationsController < Devise::RegistrationsController 
+class RegistrationsController < Devise::RegistrationsController
 
   private
 
   def sign_up_params
-  	params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 
   def acount_update_params
